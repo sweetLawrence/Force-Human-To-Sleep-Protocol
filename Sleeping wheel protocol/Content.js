80e322fa26906310d4ee49c6b49a43ab8e848977
tsungulawrence <104100594+sweetLawrence@users.noreply.github.com>
2023-01-23 22:49:00 +0300

    Add files via upload
    sleeping wheel protocol

---
+++ Sleeping wheel protocol/Content.js
@@ -0,0 +1,8 @@
+// var videoUrl = "https://www.youtube.com/watch?v=lTxn2BuqyzU"
+window.onload = () => {
+    var container = document.querySelector(".html5-video-container");
+    setTimeout(() => {
+        container.style.opacity = 0
+    }, 2000)
+
+}
